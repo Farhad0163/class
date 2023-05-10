72a45e120f1a28e95c1bbe3392422d170f5500a9
Farhad0163 <133114065+Farhad0163@users.noreply.github.com>
2023-05-10 15:55:38 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+//  Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "ddd";
+
+// Site URL
+$site_url = "http://localhost/f2"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = ""; // Your purchase code, don't give it to anyone. 
+?>
